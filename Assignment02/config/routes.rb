44a3d912a258e4aaceaa076e5e8981ed773696f3
DarkLORDCOSMOS <jsamson3@msudenver.edu>
2018-10-05 12:28:21 -0600

    Created Student and Enrollment scaffolds

--- Assignment02/config/routes.rb
+++ Assignment02/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :enrollments
+  resources :students
   resources :sections
   resources :courses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
